ad6550d8a730fd25d49d7d04ffb3a5f4ee580252
ChaMinXD <dvd3333@naver.com>
2024-04-27 16:27:24 +0900

    Design: Background Components 생성 #5

---
+++ src/components/WebBackground.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WebBackground = () => {
+  return (
+    <div className="mt-10 ml-10 w-[112rem] h-[66.9rem]  flex ">
+      <div className="w-[25.9rem] h-[66.9rem] bg-Light_Layout-100 rounded-tl-3xl"></div>
+      <div className="w-[86.1rem] h-[66.9rem] bg-Dark_Text_Contents "></div>
+    </div>
+  );
+};
+
+export default WebBackground;
