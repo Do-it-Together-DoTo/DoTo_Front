825eb6736a9f531ec7713cd56cf7dddc09d48e3b
ChaMinXD <dvd3333@naver.com>
2024-04-29 16:47:16 +0900

    Fix: rem 수치 조절 #5

--- src/components/WebBackground.tsx
+++ src/components/WebBackground.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const WebBackground = () => {
   return (
-    <div className="mt-10 ml-10 w-[112rem] h-[66.9rem]  flex ">
-      <div className="w-[25.9rem] h-[66.9rem] bg-Light_Layout-100 rounded-tl-3xl"></div>
-      <div className="w-[86.1rem] h-[66.9rem] bg-Dark_Text_Contents "></div>
+    <div className="mt-10 ml-10 w-[70rem] h-[41.8125rem]  flex ">
+      <div className="w-[16.1875rem] h-[41.8125rem] bg-Light_Layout-100 rounded-tl-3xl"></div>
+      <div className="w-[53.8125rem] h-[41.8125rem] bg-Dark_Text_Contents "></div>
     </div>
   );
 };
