1d720a51d09ce976adf6f8ccdf93c2c791e6864d
박세은 <seeunbag30@gmail.com>
2024-05-05 20:27:20 +0900

    #9 Design: 프로필&팔로워 탭 backLayer 구현

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,5 +1,13 @@
 const HomePage = () => {
-  return <div>홈화면</div>;
+  return (
+    <div className="relative flex">
+      {/* 내 프로필 정보 */}
+      <section className="absolute top-0 flex flex-col h-[calc(100vh-3.1875rem)]">
+        <div className="w-[16.1875rem] h-[10.5rem] bg-Light_Layout-100 rounded-tl-2xl dark:bg-Dark_Layout-200"></div>
+        <div className="w-[16.1875rem] bg-Light_Layout-200 h-full dark:bg-Dark_Layout-300"></div>
+      </section>
+    </div>
+  );
 };
 
 export default HomePage;
