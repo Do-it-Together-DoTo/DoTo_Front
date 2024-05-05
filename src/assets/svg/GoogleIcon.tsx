6e56550f88f845b894e82cc13966630bcb062bcb
ChaMin <kusoft1997@gmail.com>
2024-05-06 01:04:39 +0900

    Fix: Build 오류 수정

--- src/assets/svg/GoogleIcon.tsx
+++ src/assets/svg/GoogleIcon.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import type { SVGProps } from 'react';
 const SvgGoogleIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
--- src/assets/svg/KakaoIcon.tsx
+++ src/assets/svg/KakaoIcon.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import type { SVGProps } from 'react';
 const SvgKakaoIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
