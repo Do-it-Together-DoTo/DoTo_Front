cb759407c3da6fa0ef589d7e5d4161ffb18cf580
박세은 <seeunbag30@gmail.com>
2024-05-06 21:06:16 +0900

    Docs: 중복 라인 삭제

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -3,7 +3,6 @@ export { default as ArrowDownwardIcon } from './ArrowDownwardIcon';
 export { default as ArrowForwardIcon } from './community/ArrowForwardIcon';
 export { default as CommunityIcon } from './community/CommunityIcon';
 export { default as HomeIcon } from './HomeIcon';
-export { default as Logo } from './Logo';
 export { default as MoonIcon } from './MoonIcon';
 export { default as MypageIcon } from './mypage/MypageIcon';
 export { default as NotificationOffIcon } from './NotificationOffIcon';
