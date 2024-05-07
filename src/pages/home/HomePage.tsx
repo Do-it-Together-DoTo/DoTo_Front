1b03c00b0b56eb517628d7c48ea2627eb31be407
박세은 <seeunbag30@gmail.com>
2024-05-07 12:08:25 +0900

    Design: 내 프로필 UI 구현

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,9 +1,38 @@
+import { UserImgSample, CoinIcon } from '@assets/svg';
+
 const HomePage = () => {
   return (
     <div className="relative flex">
-      {/* 내 프로필 정보 */}
+      {/* 프로필 & 팔로워 */}
       <section className="absolute top-0 flex flex-col h-[calc(100vh-3.1875rem)]">
-        <div className="w-[16.1875rem] h-[10.5rem] bg-Light_Layout-100 rounded-tl-2xl dark:bg-Dark_Layout-200"></div>
+        {/* 내 프로필 */}
+        <div className="w-[16.1875rem] h-[10.5rem] bg-Light_Layout-100 rounded-tl-2xl dark:bg-Dark_Layout-200 flex flex-col justify-center relative">
+          <div className="flex items-center ml-auto mr-8 mb-0.5">
+            <CoinIcon className="w-4 h-4" />
+            <span className="font-nico text-sm text-Light_Text_Name ml-0.5">9999</span>
+          </div>
+
+          <div className="flex items-center ml-auto mr-8">
+            <div className="block w-20 h-1 rounded-full bg-Dark_Layout-100" />
+            <span className="text-[0.5rem] font-nico text-Light_Text_AboutMe mr-2 absolute right-0">99%</span>
+          </div>
+
+          <div className="relative flex justify-center mt-2.5">
+            <div className="flex flex-col items-center">
+              <span className="w-20 text-xs text-center font-nico text-Light_CategoryText_Icon_Contents">LV.3</span>
+              <UserImgSample className="absolute bottom-0 w-20 " />
+            </div>
+            <div className="flex flex-col max-w-[6.5rem] pb-4 ml-3">
+              <span className="overflow-hidden font-bold text-Light_Text_Name text-ellipsis whitespace-nowrap">
+                센센센센센센센센센센센
+              </span>
+              <span className="overflow-hidden text-xs whitespace-pre-wrap text-Light_Text_AboutMe max-h-8 text-ellipsis">
+                ssafe 1기 이 칸의 줄 수는 2줄 입니다. 높이가 일정 높이를 넘으면 생략되어야 합니다.
+              </span>
+            </div>
+          </div>
+        </div>
+        {/* 팔로워 */}
         <div className="w-[16.1875rem] bg-Light_Layout-200 h-full dark:bg-Dark_Layout-300"></div>
       </section>
     </div>
