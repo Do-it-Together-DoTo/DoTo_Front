c8a344a96ff0f42ccb23506a6b00e3bd6faaf36c
박세은 <seeunbag30@gmail.com>
2024-05-08 15:02:41 +0900

    Feat: 팔로워탭에 dnd기능 추가

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css';
 import Router from './router/Router.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <>
     <Router />
-  </React.StrictMode>,
+  </>,
 );
