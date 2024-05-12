6167e8de8b7649d75fd6127de333d8a870316db1
mung96 <mung5079@gmail.com>
2024-05-12 21:10:16 +0900

    Style: 필요없는 import문, 파일 삭제

--- src/assets/svg/community/CrownIcon.tsx
+++ src/assets/svg/community/CrownIcon.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import type { SVGProps } from 'react';
 const SvgCrownIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
--- src/assets/svg/community/RankingBackground.tsx
+++ src/assets/svg/community/RankingBackground.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import type { SVGProps } from 'react';
 const SvgRankingBackground = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 406 156" {...props}>
