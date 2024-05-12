7439eac5425b10ccfbf65251af7018afb980ad7f
박세은 <seeunbag30@gmail.com>
2024-05-12 17:47:33 +0900

    Feat: 선택된 유저 정보로 값이 바뀌는 탭 구현

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -3,10 +3,28 @@ import { Droppable, Draggable, DragDropContext, DropResult } from 'react-beautif
 import { fakerKO as faker } from '@faker-js/faker';
 import { UserImgSample, CoinIcon } from '@assets/svg';
 
+type SelectedUser = {
+  id: string;
+  username: string;
+  introduction: string;
+  level: number;
+  coin: number;
+  experience: number;
+};
+
+const myProfile = {
+  id: faker.string.uuid(),
+  username: '센센센',
+  introduction: 'ssafe 1기 이 칸의 줄 수는 2줄 입니다. 높이가 일정 높이를 넘으면 생략되어야 합니다.',
+  level: 3,
+  coin: 20,
+  experience: 84,
+};
+
 const initialFollowers = Array.from({ length: 10 }, () => ({
   id: faker.string.uuid(),
   username: faker.person.lastName() + faker.person.firstName(),
-  introduction: faker.lorem.sentence(),
+  introduction: faker.lorem.sentence({ min: 3, max: 7 }),
   level: faker.number.int({ min: 1, max: 10 }),
   coin: faker.number.int({ min: 0, max: 9999 }),
   experience: faker.number.int({ min: 0, max: 100 }),
@@ -14,6 +32,7 @@ const initialFollowers = Array.from({ length: 10 }, () => ({
 
 const HomePage = () => {
   const [followers, setFollowers] = useState(initialFollowers);
+  const [selectedUser, setSelectedUser] = useState<SelectedUser>(myProfile);
 
   const handleDragEnd = (result: DropResult) => {
     const { source, destination } = result;
@@ -30,17 +49,27 @@ const HomePage = () => {
     setFollowers(newFollowers);
   };
 
+  const onClickSelected = (props: SelectedUser) => {
+    if (Object.keys(selectedUser).length !== 0) {
+      setSelectedUser({} as SelectedUser);
+    }
+    setSelectedUser(props);
+  };
+
   return (
     <DragDropContext onDragEnd={handleDragEnd}>
       <div className="flex w-screen">
         {/* 프로필 & 팔로워 */}
         <section className="flex flex-col h-[calc(100vh-3.1875rem)]">
           {/* 내 프로필 */}
-          <div className="w-[16.1875rem] h-[10.5rem] bg-Light_Layout-100 rounded-tl-3xl dark:bg-Dark_Layout-200 flex flex-col justify-center relative">
+          <div
+            onClick={() => onClickSelected(myProfile)}
+            className="w-[16.1875rem] h-[10.5rem] bg-Light_Layout-100 rounded-tl-3xl dark:bg-Dark_Layout-200 flex flex-col justify-center relative"
+          >
             <div className="flex items-center ml-auto mr-8 mb-0.5">
               <CoinIcon className="w-4 h-4" />
               <span className="font-nico text-sm text-Light_Text_Name ml-0.5 dark:text-Dark_Text_Name">
-                {followers[0].coin}
+                {myProfile.coin}
               </span>
             </div>
 
@@ -48,27 +77,27 @@ const HomePage = () => {
               <div className="block w-20 h-1 rounded-full bg-Dark_Layout-100">
                 <div
                   className="block h-1 rounded-full bg-gradient"
-                  style={{ width: `calc(0.05rem*${followers[0].experience})` }}
+                  style={{ width: `calc(0.05rem*${myProfile.experience})` }}
                 />
               </div>
               <span className="text-[0.55rem] font-nico text-Light_Text_AboutMe mr-2 absolute right-0 dark:text-Dark_Text_AboutMe">
-                {followers[0].experience}%
+                {myProfile.experience}%
               </span>
             </div>
 
             <div className="relative flex justify-center mt-2.5">
               <div className="flex flex-col items-center">
                 <span className="w-20 text-xs text-center font-nico text-Light_CategoryText_Icon_Contents dark:text-Dark_CategoryText_Icon">
-                  LV.3
+                  LV.{myProfile.level}
                 </span>
                 <UserImgSample className="absolute bottom-0 w-20 " />
               </div>
               <div className="flex flex-col max-w-[6.5rem] pb-4 ml-3">
                 <span className="overflow-hidden font-bold text-Light_Text_Name whitespace-nowrap text-ellipsis dark:text-Dark_Text_Name">
-                  센센센센센센센센
+                  {myProfile.username}
                 </span>
                 <span className="overflow-hidden text-xs text-Light_Text_AboutMe dark:text-Dark_Text_AboutMe line-clamp-2">
-                  ssafe 1기 이 칸의 줄 수는 2줄 입니다. 높이가 일정 높이를 넘으면 생략되어야 합니다.
+                  {myProfile.introduction}
                 </span>
               </div>
             </div>
@@ -89,6 +118,7 @@ const HomePage = () => {
                           ref={provided.innerRef}
                           {...provided.draggableProps}
                           {...provided.dragHandleProps}
+                          onClick={() => onClickSelected(follower)}
                           className="flex items-center mb-5 px-5 w-[14.4375rem] h-12 rounded-md hover:bg-Light_Layout-100 active:bg-Light_Layout-100 dark:hover:bg-Dark_Layout-200 dark:active:bg-Dark_Layout-200"
                         >
                           <UserImgSample className="min-w-14 max-h-8" />
@@ -122,7 +152,7 @@ const HomePage = () => {
           <div className="flex flex-col w-3/5 h-full bg-Light_Layout-300 dark:bg-Dark_Layout-400">
             <div className="flex flex-col px-5 my-9">
               <p className="font-nico text-Light_CategoryText_Icon_Contents text-[0.625rem] h-1.5 w-40 text-right dark:text-Dark_CategoryText_Icon">
-                LV.3
+                LV.{selectedUser.level}
               </p>
               <div className="flex items-center">
                 <div className="flex w-[4.375rem] h-[4.375rem] rounded-full bg-Light_Layout-200 justify-center items-center dark:bg-Dark_Layout-300">
@@ -133,18 +163,18 @@ const HomePage = () => {
                     <div className="block w-20 h-1 rounded-full bg-Dark_Layout-100">
                       <div
                         className="block h-1 rounded-full bg-gradient"
-                        style={{ width: `calc(0.05rem*${followers[0].experience})` }}
+                        style={{ width: `calc(0.05rem*${selectedUser.experience})` }}
                       />
                     </div>
                     <span className="text-[0.55rem] font-nico text-Light_Text_AboutMe ml-0.5 dark:text-Dark_Text_AboutMe">
-                      {followers[0].experience}%
+                      {selectedUser.experience}%
                     </span>
                   </div>
                   <span className="h-4 text-base font-bold text-Light_Text_Name mb-1.5 dark:text-Dark_Text_Name">
-                    친구 1
+                    {selectedUser.username}
                   </span>
                   <span className="text-xs text-Light_Text_AboutMe dark:text-Dark_Text_AboutMe">
-                    ssafy 11기. 여기서는 좀더 길게길게 보입니다.
+                    {selectedUser.introduction}
                   </span>
                 </div>
 
