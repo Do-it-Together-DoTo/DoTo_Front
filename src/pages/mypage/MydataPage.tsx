22413ca93a19784217bfb85776f9f5b01b8b2499
ChaMin <kusoft1997@gmail.com>
2024-05-15 22:58:08 +0900

    Design: Mydata Page 다크모드 UI 구현

--- src/pages/mypage/MydataPage.tsx
+++ src/pages/mypage/MydataPage.tsx
@@ -27,115 +27,155 @@ const MydataPage = () => {
   return (
     <div className="flex flex-col items-center w-[calc(100vw-26.1875rem)] h-[calc(screen-3.1875rem)] bg-Light_Layout-200 dark:bg-Dark_Layout-300">
       <div className="mt-[2.125rem] w-[12.875rem] h-[1.875rem] flex justify-center	">
-        <ArrowLeftIcon width={30} height={30} />
-        <span className="w-[8rem] h-[1.875rem] flex justify-center items-center text-Light_Text_Name font-semibold">
+        <ArrowLeftIcon width="30" height="30" className="fill-Light_Text_Name dark:fill-Dark_Text_Name" />
+        <span className="w-[8rem] h-[1.875rem] flex justify-center items-center text-Light_Text_Name font-semibold dark:text-Dark_Text_Name">
           2024년 4월
         </span>
-        <ArrowRightIcon width={30} height={30} />
+        <ArrowRightIcon width="30" height="30" className="dark:fill-Dark_Text_Name" />
       </div>
-      <div className="mt-[2.5rem] flex flex-col items-center w-[49.25rem] h-[31.625rem] bg-Light_Layout-300 rounded-[0.9375rem] dark:bg-Dark_Layout ft-200">
-        <div className="mt-[1.875rem] w-[8.5rem] h-[1.8125rem] font-semibold text-[1.5rem]">나의 투두 기록</div>
+      <div className="mt-[2.5rem] flex flex-col items-center w-[49.25rem] h-[31.625rem] bg-Light_Layout-300 rounded-[0.9375rem] dark:bg-Dark_Layout-400">
+        <div className="mt-[1.875rem] w-[8.5rem] h-[1.8125rem] font-semibold text-[1.5rem] text-Light_Text_Name dark:text-Dark_Text_Name">
+          나의 투두 기록
+        </div>
         <div className="mt-[2.75rem] w-[44.3125rem] h-[1.625rem] flex justify-between items-center">
-          <div className="w-[5.4375rem] h-[1.5rem] text-[1.25rem] flex justify-center items-center">전체</div>
+          <div className="w-[5.4375rem] h-[1.5rem] text-[1.25rem] flex justify-center items-center text-Light_Text_Name dark:text-Dark_Text_Name">
+            전체
+          </div>
           <div className="relative w-[31.25rem] h-[1.0625rem] bg-Light_Layout-100 rounded-lg rounded-10">
             <div
               className={`absolute h-[1.0625rem] rounded-lg rounded-10 bg-Button`}
               style={{ width: graphData.all }}
             />
           </div>
-          <div className="w-[3.3125rem] h-[1.5rem] text-[1.25rem] flex justify-center items-center">
+          <div className="w-[3.3125rem] h-[1.5rem] text-[1.25rem] flex justify-center items-center dark:text-Dark_Text_Name">
             {data.all}/{initialMax.all}
           </div>
         </div>
         <div className="mt-[3.25rem] w-[44.3125rem] h-[1.625rem] flex justify-between items-center">
-          <div className="w-[5.4375rem] h-[1.5rem] text-[1.25rem] flex justify-center items-center">카테고리1</div>
-          <div className="relative w-[31.25rem] h-[1.0625rem] bg-Light_Layout-100 rounded-lg rounded-10">
+          <div className="w-[5.4375rem] h-[1.5rem] text-[1.25rem] flex justify-center items-center text-Light_Text_Name dark:text-Dark_Text_Name">
+            카테고리1
+          </div>
+          <div className="relative w-[31.25rem] h-[1.0625rem] bg-Light_Layout-100  rounded-lg rounded-10">
             <div
               className={`absolute h-[1.0625rem] rounded-lg rounded-10 bg-pink`}
               style={{ width: graphData.category1 }}
             />
           </div>
-          <div className="w-[3.3125rem] h-[1.5rem] text-[1.25rem] flex justify-center items-center">
+          <div className="w-[3.3125rem] h-[1.5rem] text-[1.25rem] flex justify-center items-center text-Light_Text_Name dark:text-Dark_Text_Name">
             {data.category1}/{initialMax.category1}
           </div>
         </div>
         <div className="mt-[3.25rem] w-[44.3125rem] h-[1.625rem] flex justify-between items-center">
-          <div className="w-[5.4375rem] h-[1.5rem] text-[1.25rem] flex justify-center items-center">카테고리2</div>
+          <div className="w-[5.4375rem] h-[1.5rem] text-[1.25rem] flex justify-center items-center text-Light_Text_Name dark:text-Dark_Text_Name">
+            카테고리2
+          </div>
           <div className="relative w-[31.25rem] h-[1.0625rem] bg-Light_Layout-100 rounded-lg rounded-10">
             <div
               className={`absolute h-[1.0625rem] rounded-lg rounded-10 bg-skyblue`}
               style={{ width: graphData.category2 }}
             />
           </div>
-          <div className="w-[3.3125rem] h-[1.5rem] text-[1.25rem] flex justify-center items-center">
+          <div className="w-[3.3125rem] h-[1.5rem] text-[1.25rem] flex justify-center items-center text-Light_Text_Name dark:text-Dark_Text_Name">
             {data.category2}/{initialMax.category2}
           </div>
         </div>
         <div className="mt-[3.25rem] w-[44.3125rem] h-[1.625rem] flex justify-between items-center">
-          <div className="w-[5.4375rem] h-[1.5rem] text-[1.25rem] flex justify-center items-center">카테고리3</div>
+          <div className="w-[5.4375rem] h-[1.5rem] text-[1.25rem] flex justify-center items-center text-Light_Text_Name dark:text-Dark_Text_Name">
+            카테고리3
+          </div>
           <div className="relative w-[31.25rem] h-[1.0625rem] bg-Light_Layout-100 rounded-lg rounded-10">
             <div
               className={`absolute h-[1.0625rem] rounded-lg rounded-10 bg-blue`}
               style={{ width: graphData.category3 }}
             />
           </div>
-          <div className="w-[3.3125rem] h-[1.5rem] text-[1.25rem] flex justify-center items-center">
+          <div className="w-[3.3125rem] h-[1.5rem] text-[1.25rem] flex justify-center items-center text-Light_Text_Name dark:text-Dark_Text_Name">
             {data.category3}/{initialMax.category3}
           </div>
         </div>
         <div className="mt-[3.25rem] w-[44.3125rem] h-[1.625rem] flex justify-between items-center">
-          <div className="w-[5.4375rem] h-[1.5rem] text-[1.25rem] flex justify-center items-center">카테고리4</div>
+          <div className="w-[5.4375rem] h-[1.5rem] text-[1.25rem] flex justify-center items-center text-Light_Text_Name dark:text-Dark_Text_Name">
+            카테고리4
+          </div>
           <div className="relative w-[31.25rem] h-[1.0625rem] bg-Light_Layout-100 rounded-lg rounded-10">
             <div
               className={`absolute h-[1.0625rem] rounded-lg rounded-10 bg-purple`}
               style={{ width: graphData.category4 }}
             />
           </div>
-          <div className="w-[3.3125rem] h-[1.5rem] text-[1.25rem] flex justify-center items-center">
+          <div className="w-[3.3125rem] h-[1.5rem] text-[1.25rem] flex justify-center items-center text-Light_Text_Name dark:text-Dark_Text_Name">
             {data.category4}/{initialMax.category4}
           </div>
         </div>
       </div>
-      <div className="mt-[2.5rem] flex flex-col items-center w-[49.25rem] h-[45.8125rem] bg-Light_Layout-300 rounded-[0.9375rem] dark:bg-Dark_Layout ft-200">
-        <div className="mt-[1.875rem] w-[8.5rem] h-[1.8125rem] font-semibold text-[1.5rem]">나의 코인 기록</div>
+      <div className="mt-[2.5rem] flex flex-col items-center w-[49.25rem] h-[45.8125rem] bg-Light_Layout-300 rounded-[0.9375rem] dark:bg-Dark_Layout-400">
+        <div className="mt-[1.875rem] w-[8.5rem] h-[1.8125rem] font-semibold text-[1.5rem] text-Light_Text_Name dark:text-Dark_Text_Name">
+          나의 코인 기록
+        </div>
         <div className="mt-[3.8125rem] w-[41.125rem] h-[9.375rem] flex justify-between">
-          <div className="w-[19.8125rem] h-[9.375rem] flex flex-col items-center bg-Light_Layout-400 rounded-lg">
+          <div className="w-[19.8125rem] h-[9.375rem] flex flex-col items-center bg-Light_Layout-400 dark:bg-Dark_Layout-300 rounded-lg">
             <div className="mt-[1.875rem] w-[13.8125rem] h-[2.25rem] flex justify-between">
               <div className="w-[4.875rem] h-[2.25rem] flex justify-center items-center">
                 <div className="w-[1.125rem] h-[1.125rem] bg-Button rounded-full"></div>
-                <div className="ml-[0.4375rem] flex justify-center items-center w-[3.25rem] h-[2.25rem] font-semibold text-[1.25rem] text-Light_Text_AboutMe">
+                <div className="ml-[0.4375rem] flex justify-center items-center w-[3.25rem] h-[2.25rem] font-semibold text-[1.25rem] text-Light_Text_AboutMe dark:text-Dark_Text_AboutMe">
                   사용량
                 </div>
               </div>
               <div className="w-[4.875rem] h-[2.25rem] flex justify-center items-center">
                 <div className="font-semibold text-Button text-[2rem]">134</div>
-                <div className="font-semibold text-Light_Text_Name text-[1.25rem]">개</div>
+                <div className="font-semibold text-Light_Text_Name dark:text-Dark_Text_Name text-[1.25rem]">개</div>
               </div>
             </div>
             <div className="mt-[0.875rem] w-[13.8125rem] h-[2.25rem] flex justify-between">
               <div className="w-[4.875rem] h-[2.25rem] flex justify-center items-center">
                 <div className="w-[1.125rem] h-[1.125rem] bg-Light_Layout-100 rounded-full"></div>
-                <div className="ml-[0.4375rem] flex justify-center items-center w-[3.25rem] h-[2.25rem] font-semibold text-[1.25rem] text-Light_Text_AboutMe">
+                <div className="ml-[0.4375rem] flex justify-center items-center w-[3.25rem] h-[2.25rem] font-semibold text-[1.25rem] text-Light_Text_AboutMe dark:text-Dark_Text_AboutMe">
                   획득량
                 </div>
               </div>
               <div className="w-[4.875rem] h-[2.25rem] flex justify-center items-center">
                 <div className="font-semibold text-Button text-[2rem]">345</div>
-                <div className="font-semibold text-Light_Text_Name text-[1.25rem]">개</div>
+                <div className="font-semibold text-Light_Text_Name dark:text-Dark_Text_Name text-[1.25rem]">개</div>
               </div>
             </div>
           </div>
-          <div className="w-[19.8125rem] h-[9.375rem] flex flex-col items-center bg-Light_Layout-400 rounded-lg">
-            <div className="mt-[1.75rem] text-[1.25rem] font-semibold text-Light_Text_AboutMe">내가 연 베팅</div>
+          <div className="w-[19.8125rem] h-[9.375rem] flex flex-col items-center bg-Light_Layout-400 dark:bg-Dark_Layout-300 rounded-lg">
+            <div className="mt-[1.75rem] text-[1.25rem] font-semibold text-Light_Text_AboutMe dark:text-Dark_Text_AboutMe">
+              내가 연 베팅
+            </div>
             <div className="mt-[1rem] flex justify-center items-center">
               <div className="font-semibold text-Button text-[2.25rem]">345</div>
-              <div className="font-semibold text-Light_Text_Name text-[2.25rem]">회</div>
+              <div className="font-semibold text-Light_Text_Name dark:text-Dark_Text_Name text-[2.25rem]">회</div>
             </div>
           </div>
         </div>
         <div className="mt-[2.125rem] w-[41.125rem] h-[23.8125rem] flex justify-between">
-          <div className="w-[19.8125rem] h-[23.8125rem] flex flex-col items-center bg-Light_Layout-400 rounded-lg"></div>
-          <div className="w-[19.8125rem] h-[23.8125rem] flex flex-col items-center bg-Light_Layout-400 rounded-lg"></div>
+          <div className="w-[19.8125rem] h-[23.8125rem] flex flex-col items-center bg-Light_Layout-400 dark:bg-Dark_Layout-300 rounded-lg">
+            <div className="mt-[1rem] font-semibold text-[1.25rem] text-Light_Text_AboutMe dark:text-Dark_Text_AboutMe">
+              베팅 승률
+            </div>
+            <div className="w-[13.75rem] h-[13.75rem]">도넛 테이블</div>
+            <div className="w-[4.875rem] h-[2.25rem] flex justify-center items-center">
+              <div className="font-semibold text-Button text-[2rem]">38</div>
+              <div className="font-semibold text-Light_Text_Name dark:text-Dark_Text_Name text-[2rem]">%</div>
+            </div>
+            <div className="font-semibold text-[1.25rem] text-Light_Text_AboutMe dark:text-Dark_Text_AboutMe">
+              4회 참여
+            </div>
+          </div>
+          <div className="w-[19.8125rem] h-[23.8125rem] flex flex-col items-center bg-Light_Layout-400 dark:bg-Dark_Layout-300 rounded-lg">
+            <div className="mt-[1rem] font-semibold text-[1.25rem] text-Light_Text_AboutMe dark:text-Dark_Text_AboutMe">
+              베팅 수익률
+            </div>
+            <div className="w-[13.75rem] h-[13.75rem]">도넛 테이블</div>
+            <div className="w-[4.875rem] h-[2.25rem] flex justify-center items-center">
+              <div className="font-semibold text-Button text-[2rem]">38</div>
+              <div className="font-semibold text-Light_Text_Name dark:text-Dark_Text_Name text-[2rem]">%</div>
+            </div>
+            <div className="font-semibold text-[1.25rem] text-Light_Text_AboutMe dark:text-Dark_Text_AboutMe">
+              182개 획득
+            </div>
+          </div>
         </div>
       </div>
     </div>
