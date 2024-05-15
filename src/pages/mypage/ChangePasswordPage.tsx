aac8885279aad625519959ab9ada9f7a873c7f06
ChaMin <kusoft1997@gmail.com>
2024-05-15 19:29:14 +0900

    Fix: 비빌번호 변경 페이지 UI 색상 변경

--- src/pages/mypage/ChangePasswordPage.tsx
+++ src/pages/mypage/ChangePasswordPage.tsx
@@ -5,7 +5,7 @@ const ChangePasswordPage = () => {
       <h1 className="mt-[3.25rem] h-[2rem] text-Light_CategoryText_Icon_Contents font-semibold text-[1.625rem] dark:text-Dark_Text_Name">
         비밀번호 변경
       </h1>
-      <div className="mt-[5.5rem] flex flex-col w-[47.875rem] h-[32.25rem]  bg-Light_Layout-100 rounded-lg rounded-[0.9375rem] dark:bg-Dark_Layout-200">
+      <div className="mt-[5.5rem] flex flex-col w-[47.875rem] h-[32.25rem]  bg-Light_Layout-300 rounded-lg rounded-[0.9375rem] dark:bg-Dark_Layout-200">
         <div className="mt-[5.6875rem] flex w-[33.375rem] h-[3rem] ml-[8.125rem]">
           <label
             className="w-[6.6875rem] h-[1.5rem] font-pre text-[1.25rem] text-Light_Text_Name dark:text-Dark_Text_Name tracking-[-0.02rem]"
@@ -15,7 +15,7 @@ const ChangePasswordPage = () => {
           </label>
           <div className="w-[25.0625rem] ml-[1.625rem]">
             <input
-              className="w-[24.375rem] outline-none bg-Light_Layout-100 placeholder-Dark_Text_Contents dark:placeholder-Dark_Text_AboutMe text-[0.85rem] dark:bg-Dark_Layout-200 dark:text-Dark_Text_Name"
+              className="w-[24.375rem] outline-none bg-Light_Layout-300 placeholder-Dark_Text_Contents dark:placeholder-Dark_Text_AboutMe text-[0.85rem] dark:bg-Dark_Layout-200 dark:text-Dark_Text_Name"
               placeholder="현재 비밀번호"
               type="text"
               name="nowPw"
@@ -33,7 +33,7 @@ const ChangePasswordPage = () => {
           </label>
           <div className="w-[25.0625rem] ml-[1.625rem]">
             <input
-              className="w-[24.375rem] outline-none bg-Light_Layout-100 placeholder-Dark_Text_Contents dark:placeholder-Dark_Text_AboutMe text-[0.85rem] dark:bg-Dark_Layout-200 dark:text-Dark_Text_Name"
+              className="w-[24.375rem] outline-none bg-Light_Layout-300 placeholder-Dark_Text_Contents dark:placeholder-Dark_Text_AboutMe text-[0.85rem] dark:bg-Dark_Layout-200 dark:text-Dark_Text_Name"
               placeholder="새 비밀번호"
               type="text"
               name="newPw"
@@ -53,7 +53,7 @@ const ChangePasswordPage = () => {
           </label>
           <div className="w-[25.0625rem] ml-[1.625rem]">
             <input
-              className="w-[24.375rem] outline-none bg-Light_Layout-100 placeholder-Dark_Text_Contents dark:placeholder-Dark_Text_AboutMe text-[0.85rem] dark:bg-Dark_Layout-200 dark:text-Dark_Text_Name"
+              className="w-[24.375rem] outline-none bg-Light_Layout-300 placeholder-Dark_Text_Contents dark:placeholder-Dark_Text_AboutMe text-[0.85rem] dark:bg-Dark_Layout-200 dark:text-Dark_Text_Name"
               placeholder="비밀번호 확인"
               type="text"
               name="newPwCheck"
