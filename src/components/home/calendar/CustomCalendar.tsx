ff8ef4354a714859025a106bd8c1f981643caecd
박세은 <seeunbag30@gmail.com>
2024-05-15 19:51:06 +0900

    #18 Feat: 캘린더 dark모드 추가

--- src/components/home/calendar/CustomCalendar.tsx
+++ src/components/home/calendar/CustomCalendar.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Calendar from 'react-calendar';
 import moment from 'moment';
 import './CustomCalendar.css';
+import useDarkModeStore from '@/store/darkmodeStore';
 import { PrevLabel, NextLabel } from '@assets/svg/home/calendar';
 
 type ValuePiece = Date | null;
@@ -11,10 +12,11 @@ type Value = ValuePiece | [ValuePiece, ValuePiece];
 const CustomCalendar = () => {
   const [value, onChange] = useState<Value>(new Date());
   const [selectedMonth, setSelectedMonth] = useState(new Date().getMonth());
+  const isDarkMode = useDarkModeStore((state) => state.isDarkMode);
 
   const addContent = ({ date, view }: { date: Date; view: string }) => {
     if (view === 'month' && date.getMonth() === selectedMonth) {
-      return <div className="w-8 h-8 rounded-md bg-Light_Layout-300"></div>;
+      return <div className="w-8 h-8 rounded-md bg-Light_Layout-300 dark:bg-Light_Text_AboutMe"></div>;
     }
   };
 
@@ -24,14 +26,15 @@ const CustomCalendar = () => {
 
   return (
     <Calendar
+      className={isDarkMode ? 'dark' : ''}
+      locale="ko"
       onChange={onChange}
       value={value}
       minDetail="month"
       next2Label={null}
       prev2Label={null}
-      nextLabel={<NextLabel />}
-      prevLabel={<PrevLabel />}
-      locale="ko"
+      nextLabel={<NextLabel {...(isDarkMode ? { fill: '#F2F2F2' } : {})} />}
+      prevLabel={<PrevLabel {...(isDarkMode ? { fill: '#F2F2F2' } : {})} />}
       formatDay={(_locale, date) => moment(date).format('D')}
       onActiveStartDateChange={({ activeStartDate }) => {
         if (activeStartDate) {
