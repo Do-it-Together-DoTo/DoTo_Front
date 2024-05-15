7a562a396e525903af9fc61e7b4b917b4d031ec7
mung96 <mung5079@gmail.com>
2024-05-15 13:15:31 +0900

    Feat: 채팅 Enter 이벤트 지정

--- src/pages/community/ChattingRoomPage.tsx
+++ src/pages/community/ChattingRoomPage.tsx
@@ -1,5 +1,5 @@
 import ChattingMessage from '@/components/community/chatting/ChattingMessage';
-import { useEffect, useState } from 'react';
+import { FormEvent, KeyboardEvent, useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import {
   CHATTING_ROOM_LIST,
@@ -18,6 +18,20 @@ const ChattingRoomPage = () => {
     setMessages(CHATTING_MESSAGE_LIST);
   }, [roomId]);
 
+  const formRef = useRef<HTMLFormElement>(null);
+  const handleEnterPress = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key == 'Enter' && !e.shiftKey) {
+      e.preventDefault();
+      formRef.current?.dispatchEvent(new Event('submit', { bubbles: true, cancelable: true }));
+      (e.target as HTMLTextAreaElement).value = '';
+      // TODO:초기화과정 다르게 해야함
+    }
+  };
+  const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // TODO: 소켓요청
+  };
+
   return (
     <main className="w-full  h-[calc(100vh-3.1875rem)] flex  flex-col gap-6 bg-Light_Layout-200 dark:bg-Dark_Layout-300 items-center py-14">
       <section className="flex flex-col w-[33.75rem] py-8 px-9 bg-Light_Layout-400 gap-6 rounded-2xl dark:bg-Dark_Layout-400">
@@ -35,12 +49,18 @@ const ChattingRoomPage = () => {
               <ChattingMessage message={message} />
             ))}
           </div>
-          <div className="pl-2 py-3 bg-Light_Layout-400 rounded-[0.625rem] dark:bg-Dark_Layout-400">
+          <form
+            ref={formRef}
+            onSubmit={handleFormSubmit}
+            className="pl-2 py-3 bg-Light_Layout-400 rounded-[0.625rem] dark:bg-Dark_Layout-400"
+          >
             <textarea
               rows={4}
               className="w-full bg-Light_Layout-400 resize-none outline-none dark:bg-Dark_Layout-400"
+              onKeyDown={handleEnterPress}
             />
-          </div>
+            <button type="submit">전송하기</button>
+          </form>
         </div>
       </section>
     </main>
