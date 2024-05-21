9e9bd102e23d51ffa0b3a015b5074c4d7b5ce10b
ChaMin <kusoft1997@gmail.com>
2024-05-21 23:53:44 +0900

    Design: Block Account Page 다크모드 UI 적용

--- src/pages/mypage/BlockAccountPage.tsx
+++ src/pages/mypage/BlockAccountPage.tsx
@@ -6,7 +6,7 @@ const BlockAccountPage = () => {
       <h1 className="mt-[3.25rem] h-[2rem] text-Light_CategoryText_Icon_Contents font-semibold text-[1.625rem] dark:text-Dark_Text_Name">
         차단 목록
       </h1>
-      <div className="mt-[3rem] flex flex-col items-center w-[25.6875rem] h-[28.125rem] bg-Light_Layout-300 rounded-[2rem]">
+      <div className="mt-[3rem] flex flex-col items-center w-[25.6875rem] h-[28.125rem] bg-Light_Layout-300 dark:bg-Dark_Layout-400 rounded-[2rem]">
         <div className="mt-[2.5rem] w-[21.875rem] h-[3rem] flex justify-evenly items-center">
           <div className="  relative w-[2.5rem] h-[2.5rem] bg-Light_Layout-100 rounded-full dark:bg-Dark_Layout-100">
             <UserImgSample
@@ -15,7 +15,9 @@ const BlockAccountPage = () => {
               className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
             />
           </div>
-          <div className="flex justify-center items-center text-[0.875rem]">닉네임 뭐하지</div>
+          <div className="flex justify-center items-center text-[0.875rem] text-Light_CategoryText_Icon_Contents dark:text-Dark_CategoryText_Icon">
+            닉네임 뭐하지
+          </div>
           <button className="w-[5.75rem] h-[1.5625rem] bg-Light_Layout-100 font-pre text-Light_Text_AboutMe text-[0.8rem] rounded-[1.5625rem]">
             변경
           </button>
