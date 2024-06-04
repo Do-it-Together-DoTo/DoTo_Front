2e31ebec33f4ed2e73aca556acc71213bc70fc60
doreming16 <jjhoneyyyy@gmail.com>
2024-06-05 08:53:12 +0900

    design) item box scroll 전체 적용

--- src/pages/store/InventoryCharacterPage.tsx
+++ src/pages/store/InventoryCharacterPage.tsx
@@ -3,27 +3,29 @@ import StoreMainProfile from '@/components/store/StoreMainProfile';
 
 const InventoryCharacterPage = () => {
   return (
-    <div className="h-[calc(100vh-3.1875rem)] overflow-auto scroll scrollbar-hide bg-Light_Layout-200 dark:bg-Dark_Layout-300 grow">
+    <div className="h-[calc(100vh-3.1875rem)] bg-Light_Layout-200 dark:bg-Dark_Layout-300 grow">
       <div className="h-[5.625rem] flex items-center justify-center text-[1.625rem] text-Light_CategoryText_Icon_Contents">
         보관함
       </div>
-      <div className="px-[1.875rem]">
+      <div className="flex flex-col px-[1.875rem] h-[calc(100vh-8.8125rem)]">
         <StoreMainProfile />
-        <div className="flex flex-wrap gap-x-[1.75rem] gap-y-[1.25rem] w-full py-[2.3125rem]">
-          <InventoryCharacter />
-          <InventoryCharacter />
-          <InventoryCharacter />
-          <InventoryCharacter />
-          <InventoryCharacter />
-          <InventoryCharacter />
-          <InventoryCharacter />
-          <InventoryCharacter />
-          <InventoryCharacter />
-          <InventoryCharacter />
-          <InventoryCharacter />
-          <InventoryCharacter />
-          <InventoryCharacter />
-          <InventoryCharacter />
+        <div className="flex-1 my-[2.3125rem] overflow-auto scroll">
+          <div className="flex flex-wrap gap-x-[1.75rem] gap-y-[1.25rem] w-full">
+            <InventoryCharacter />
+            <InventoryCharacter />
+            <InventoryCharacter />
+            <InventoryCharacter />
+            <InventoryCharacter />
+            <InventoryCharacter />
+            <InventoryCharacter />
+            <InventoryCharacter />
+            <InventoryCharacter />
+            <InventoryCharacter />
+            <InventoryCharacter />
+            <InventoryCharacter />
+            <InventoryCharacter />
+            <InventoryCharacter />
+          </div>
         </div>
       </div>
     </div>
--- src/pages/store/InventoryItemPage.tsx
+++ src/pages/store/InventoryItemPage.tsx
@@ -3,30 +3,32 @@ import StoreMainProfile from '@/components/store/StoreMainProfile';
 
 const InventoryItemPage = () => {
   return (
-    <div className="h-[calc(100vh-3.1875rem)] overflow-auto scroll scrollbar-hide bg-Light_Layout-200 dark:bg-Dark_Layout-300 grow">
+    <div className="h-[calc(100vh-3.1875rem)] bg-Light_Layout-200 dark:bg-Dark_Layout-300 grow">
       <div className="h-[5.625rem] flex items-center justify-center text-[1.625rem] text-Light_CategoryText_Icon_Contents">
         보관함
       </div>
-      <div className="px-[1.875rem] w-full">
+      <div className="flex flex-col px-[1.875rem] h-[calc(100vh-8.8125rem)]">
         <StoreMainProfile />
-        <div className="flex flex-wrap gap-x-[1.75rem] gap-y-[1.25rem] w-full py-[2.3125rem]">
-          <InventoryItem itemName={'특 성장 물약'} isRare={true} itemValue={8} />
-          <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
-          <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
-          <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
-          <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
-          <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
-          <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
-          <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
-          <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
-          <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
-          <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
-          <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
-          <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
-          <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
-          <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
-          <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
-          <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
+        <div className="flex-1 my-[2.3125rem] overflow-auto scroll">
+          <div className="flex flex-wrap gap-x-[1.75rem] gap-y-[1.25rem] w-full py-[2.3125rem]">
+            <InventoryItem itemName={'특 성장 물약'} isRare={true} itemValue={8} />
+            <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
+            <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
+            <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
+            <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
+            <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
+            <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
+            <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
+            <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
+            <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
+            <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
+            <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
+            <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
+            <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
+            <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
+            <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
+            <InventoryItem itemName={'성장 물약'} isRare={false} itemValue={3} />
+          </div>
         </div>
       </div>
     </div>
