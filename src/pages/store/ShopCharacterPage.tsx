8529e3187dbcf714c48d6335edd7ea0af08880be
doreming16 <jjhoneyyyy@gmail.com>
2024-06-05 08:50:12 +0900

    design) item box만 스크롤되게 변경

--- src/pages/store/ShopCharacterPage.tsx
+++ src/pages/store/ShopCharacterPage.tsx
@@ -4,33 +4,34 @@ import StoreMainProfile from '@/components/store/StoreMainProfile';
 
 const ShopItemPage = () => {
   return (
-    <div className="h-[calc(100vh-3.1875rem)] overflow-auto scroll scrollbar-hide bg-Light_Layout-200 dark:bg-Dark_Layout-300 grow">
+    <div className="h-[calc(100vh-3.1875rem)] bg-Light_Layout-200 dark:bg-Dark_Layout-300 grow">
       <div className="h-[5.625rem] flex items-center justify-center text-[1.625rem] text-Light_CategoryText_Icon_Contents">
         스토어
       </div>
-      {/* <div className="flex flex-col items-center px-[1.875rem]"> */}
-      <div className="px-[1.875rem]">
+      {/* 스크롤할 div height 정확하게 지정 */}
+      <div className="flex flex-col px-[1.875rem] h-[calc(100vh-8.8125rem)]">
         <StoreMainProfile />
-        <div className="flex flex-wrap gap-x-[1.75rem] gap-y-[1.25rem] w-full py-[2.3125rem]">
-          {/* <div className="flex flex-wrap gap-x-[1.75rem] gap-y-[1.25rem] w-[49.1875rem] py-[2.3125rem]"> */}
-          <ShopCharacter characterName={'캐릭터1'} coinValue={200} />
-          <ShopCharacter characterName={'캐릭터2'} coinValue={300} />
-          <ShopCharacter characterName={'캐릭터3'} coinValue={400} />
-          <ShopCharacter characterName={'캐릭터3'} coinValue={400} />
-          <ShopCharacter characterName={'캐릭터3'} coinValue={400} />
-          <ShopCharacter characterName={'캐릭터3'} coinValue={400} />
-          <ShopCharacter characterName={'캐릭터3'} coinValue={400} />
-          <ShopCharacter characterName={'캐릭터3'} coinValue={400} />
-          <ShopCharacter characterName={'캐릭터3'} coinValue={400} />
-          <ShopCharacter characterName={'캐릭터3'} coinValue={400} />
-          <ShopCharacter characterName={'캐릭터3'} coinValue={400} />
-          <ShopCharacter characterName={'캐릭터3'} coinValue={400} />
-          <ShopCharacter characterName={'캐릭터3'} coinValue={400} />
-          <ShopCharacter characterName={'캐릭터3'} coinValue={400} />
-          <ShopCharacter characterName={'캐릭터3'} coinValue={400} />
-        </div>
-        {/* <h3>Modal UI Test</h3>
+        <div className="flex-1 my-[2.3125rem] overflow-auto scroll">
+          <div className="flex flex-wrap gap-x-[1.75rem] gap-y-[1.25rem] w-full">
+            <ShopCharacter characterName={'캐릭터1'} coinValue={200} />
+            <ShopCharacter characterName={'캐릭터2'} coinValue={300} />
+            <ShopCharacter characterName={'캐릭터3'} coinValue={400} />
+            <ShopCharacter characterName={'캐릭터3'} coinValue={400} />
+            <ShopCharacter characterName={'캐릭터3'} coinValue={400} />
+            <ShopCharacter characterName={'캐릭터3'} coinValue={400} />
+            <ShopCharacter characterName={'캐릭터3'} coinValue={400} />
+            <ShopCharacter characterName={'캐릭터3'} coinValue={400} />
+            <ShopCharacter characterName={'캐릭터3'} coinValue={400} />
+            <ShopCharacter characterName={'캐릭터3'} coinValue={400} />
+            <ShopCharacter characterName={'캐릭터3'} coinValue={400} />
+            <ShopCharacter characterName={'캐릭터3'} coinValue={400} />
+            <ShopCharacter characterName={'캐릭터3'} coinValue={400} />
+            <ShopCharacter characterName={'캐릭터3'} coinValue={400} />
+            <ShopCharacter characterName={'캐릭터3'} coinValue={400} />
+          </div>
+          {/* <h3>Modal UI Test</h3>
         <StoreDetailModal /> */}
+        </div>
       </div>
     </div>
   );
