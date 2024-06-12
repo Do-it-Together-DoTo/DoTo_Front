258806e8f794b18e2d8b85ce3c337b7cc9bace23
mung96 <mung5079@gmail.com>
2024-06-12 10:40:16 +0900

    Fix: navbar선택시 색상 안변하는 버그 수정

--- src/assets/svg/MypageIcon.tsx
+++ src/assets/svg/MypageIcon.tsx
@@ -2,7 +2,6 @@ import type { SVGProps } from 'react';
 const SvgMypageIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 28 28" {...props}>
     <path
-      fill="#F2F2F2"
       fillRule="evenodd"
       d="M25.2 14c0 6.186-5.015 11.2-11.2 11.2-6.186 0-11.2-5.014-11.2-11.2S7.814 2.8 14 2.8 25.2 7.814 25.2 14m-8.4-4.2a2.8 2.8 0 1 1-5.6 0 2.8 2.8 0 0 1 5.6 0M14 15.4a7 7 0 0 0-6.365 4.082A8.38 8.38 0 0 0 14 22.4a8.38 8.38 0 0 0 6.364-2.918A7 7 0 0 0 14 15.4"
       clipRule="evenodd"
