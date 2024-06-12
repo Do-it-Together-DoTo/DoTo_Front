996e1c823d4f92fe9056afba89a942e70b8f7c6c
mung96 <mung5079@gmail.com>
2024-06-12 10:28:02 +0900

    Fix: husky 오류 수정

--- src/assets/svg/ArrowLeftIcon.tsx
+++ src/assets/svg/ArrowLeftIcon.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import type { SVGProps } from 'react';
 const SvgArrowLeftIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 25" {...props}>
